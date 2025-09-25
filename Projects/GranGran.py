152a1999c98d0abe329bba15a461c78ef902dc77
Gottem <gautamtrehan@seattleacademy.org>
2025-09-25 02:37:11 +0000

    SUBMITTED

---
+++ Projects/GranGran.py
@@ -0,0 +1,11 @@
+while True:
+    word = input("What do you think Gran Gran likes")
+
+    if "f" in word:
+        print(f"Gran Gran LIKES {word}")
+    else:
+        print(f" Gran Gran DOESNT LIKE {word}")
+
+    print("")
+
+    
